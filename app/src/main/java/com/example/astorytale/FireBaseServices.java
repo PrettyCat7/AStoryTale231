8db2477ab9100e18bd2be8280e119a719166ed87
maram <barbourmaram@gmail.com>
2023-11-11 09:29:04 +0200

    Initial commit

---
+++ app/src/main/java/com/example/astorytale/FireBaseServices.java
@@ -0,0 +1,4 @@
+package com.example.astorytale;
+
+public class FireBaseServices {
+}
---
+++ app/src/main/java/com/example/astorytale/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.astorytale;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
